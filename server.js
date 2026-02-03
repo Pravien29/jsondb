87b37e79aeee6b302025a3fdc04cdc2fe7384c28
Pravien29 <pravien072005@gmail.com>
2026-02-03 12:20:55 +0530

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import jsonServer from "json-server";
 
 const server = jsonServer.create();
-const router = jsonServer.router("cart.json");  // your json file name
+const router = jsonServer.router("news.json"); 
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 4000;
